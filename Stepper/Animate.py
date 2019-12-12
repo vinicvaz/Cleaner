295585dacf16efe2b5d03132f499b807d8cfcbd1
vinicvaz <vinicvaz95@gmail.com>
2019-12-12 19:11:17 -0300

    Animate Plot

--- Stepper/Animate.py
+++ Stepper/Animate.py
@@ -1,22 +1,36 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from Reader import Reader_Class
+from Tracking import Tracking_Class
+import threading
+
+class Plot_Class:
+
+	def __init__(self):
+
+		#### Start Tracking Class and run as Background ###
+		self.tracking = Tracking_Class()
+		print("Iniciando Thread")
+		self.thread_tracking = threading.Thread(target = self.tracking.tracking)
+		self.thread_tracking.daemon = True
+		self.thread_tracking.start()
+
+		print("Passei Da Thread")
+
+		self.ani = FuncAnimation(plt.gcf(), self.animate, interval=0.0001)
+
+	def animate(self,i):
+		x = self.tracking.pos_dict['x']
+		y = self.tracking.pos_dict['y']
+		plt.cla()
+		plt.plot(x,y) 
+
+	
 
 
 
 
-def animate(i):
-	reader.update_data()
-	x = reader.distance
-	y = reader.pitch
+plot = Plot_Class()
 
-	plt.cla()
-	plt.plot(x,y)
-
-reader = Reader_Class()
-#plot = Animate_Plot()
-
-ani = FuncAnimation(plt.gcf(), animate, interval=0.001)
 plt.tight_layout()
 plt.show()
